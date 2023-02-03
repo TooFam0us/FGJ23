9ff595798465a28b6cc2bf6f62b56ac8dcf0a0dd
kimierik <kimi.malkamaki@gmail.com>
2023-02-03 22:42:19 +0200

    should work character controller

--- FGJProject/Assets/Scripts/Feature_script.cs
+++ FGJProject/Assets/Scripts/Feature_script.cs
@@ -7,7 +7,7 @@ public class Feature_script : MonoBehaviour {
     //has data on what hair is on this character what color it is
     //also knows if this is one of the parents
 
-    bool IsParent;
+    public bool IsParent;
 
     //incase we need this 
     bool IsMale;
