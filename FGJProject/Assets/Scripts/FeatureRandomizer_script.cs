55b08e10a945dfb72e5502ad8823136a22d754eb
kimierik <kimi.malkamaki@gmail.com>
2023-02-04 02:55:07 +0200

    more materials, random haircolor, something else i forget

--- FGJProject/Assets/Scripts/FeatureRandomizer_script.cs
+++ FGJProject/Assets/Scripts/FeatureRandomizer_script.cs
@@ -7,8 +7,8 @@ public class FeatureRandomizer_script : MonoBehaviour
 {
 
     List<Mesh> HairStyle_List=new List<Mesh>();
+    List<Material> ColorMaterial_List=new List<Material>();
 
-    //list of hair meshes
     //list of eyecols
     //list of skincols
     //list of cols in generall i guess
@@ -27,9 +27,10 @@ public class FeatureRandomizer_script : MonoBehaviour
     // Start is called before the first frame update
     void Start() {
 
+        PopulateMaterialColorList();
         PopulateHairMeshList();
         //gib player hair  color 
-        player.GetComponent<Feature_script>().SetFeatures(GetRandomHair(),GetMaterialFromColorEnum(Color_enum.Green),null,null,false);
+        player.GetComponent<Feature_script>().SetFeatures(GetRandomHair(),GetRandomColor(),null,null,false);
         
     }
 
@@ -38,7 +39,7 @@ public class FeatureRandomizer_script : MonoBehaviour
 
     void PopulateHairMeshList(){
 
-        //hyvin likaista, hyvin epätehokasta
+        //hyvin likaista, hyvin epätehokasta kopioitusa
         string[] guids = AssetDatabase.FindAssets( "t:Mesh",new string[]{"Assets/Art/Models/Character/Hair"});
         //string[] guids = AssetDatabase.FindAssets("hamehair" );
 
@@ -52,54 +53,27 @@ public class FeatureRandomizer_script : MonoBehaviour
 
     Mesh GetRandomHair(){
         int max = HairStyle_List.Count; 
-        Debug.Log(max);
         return HairStyle_List[Random.Range(0,max)] ;
     }
 
+    Material GetRandomColor(){
+        int max = ColorMaterial_List.Count; 
+        return ColorMaterial_List[Random.Range(0,max)] ;
+    }
 
-    Material GetMatByName(string name){
+
+    void PopulateMaterialColorList(){
 
         //hyvin likaista, hyvin epätehokasta
-        Material[] matarr={null};
-        string[] guids = AssetDatabase.FindAssets( name,new string[]{"Assets/Materials/FeatureColorMaterials"});
+        string[] guids = AssetDatabase.FindAssets( "t:Material",new string[]{"Assets/Materials/FeatureColorMaterials"});
 
         foreach( var guid in guids ) {
             var path = AssetDatabase.GUIDToAssetPath( guid );
             Material mat = AssetDatabase.LoadAssetAtPath<Material>( path );
-            matarr[0]=mat;
+            ColorMaterial_List.Add(mat);
         }
-        return matarr[0];
     }
 
-    /* helper fn to fet matarial from color enum*/
-    Material GetMaterialFromColorEnum(Color_enum color){
-
-
-        switch (color) {
-            case Color_enum.Green:
-                Debug.Log("green");
-                return GetMatByName("GreenFeature");
-
-            case Color_enum.Red:
-                Debug.Log("red");
-                return GetMatByName("RedFeature");
-
-            case Color_enum.Blue:
-                Debug.Log("blue");
-                return GetMatByName("BlueFeature");
-
-            case Color_enum.Blonde:
-                Debug.Log("blone");
-                return GetMatByName("BlondeFeature");
-            
-            case Color_enum.Yellow:
-                Debug.Log("yellow");
-                return GetMatByName("YellowFeature");
-            
-            default:
-                return null;
-       } 
-    }
 
 
     void RandomizeFeaturesOfGo(GameObject go){
@@ -108,6 +82,7 @@ public class FeatureRandomizer_script : MonoBehaviour
             Debug.Log("Gameobject given to feature randomizer does not contain the feature_script component");
         }else{
             //randomize the features of go
+
 
         }
     }
